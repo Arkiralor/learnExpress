15c2cb5d27e5b5e9c98f9405f6d155ff14e48c60
Arkiralor <prithoo11335@gmail.com>
2023-12-24 17:19:07 +0530

    user creation controller and route working as intended.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { JS_IND } from "../constants.js"
 import { ApiError } from "../utils/apiError.js"
+import {ApiResponse} from "../utils/apiResponse.js"
 import { userModel } from "../models/user.models.js"
+import { uploadToCloudinary } from "../utils/cloudinaryHandler.js"
 
 const registerUser = asyncHandler(async (req, res) => {
 
@@ -27,18 +29,83 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const { username, email, fullName, password } = req.body
 
-        if (
-            [username, email, password, fullName].some((field) => field?.trim() === "")
-        ){
-            throw new ApiError(
-                400,
-                "All fields are required."
-            )
-        }
-        res.status(200).json(req.body)
+    if (
+        [username, email, password, fullName].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(
+            400,
+            "All fields are required."
+        )
+    }
+
+    const existingUser = await userModel.findOne({
+        $or: [
+            {
+                username
+            },
+            {
+                email
+            }
+        ]
+    })
+
+    if (existingUser) {
+        throw new ApiError(
+            409,
+            `A user with the username: ${username} or email: ${email} already exists.`
+        )
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(
+            404,
+            "Avatar is required."
+        )
+    }
+
+    const avatarCloud = await uploadToCloudinary(avatarLocalPath)
+    const coverImageCloud = await uploadToCloudinary(coverImageLocalPath)
+    
+
+    if (!avatarCloud) {
+        throw new ApiError(
+            500,
+            "File was not successfully uploaded."
+        )
+    }
+
+    const userObj = {
+        username: username.toLowerCase(),
+        email,
+        password,
+        fullName,
+        avatar: avatarCloud.url,
+        coverImage: coverImageCloud.url || ""
+    }
+
+    const user = await userModel.create(userObj)
+    
+    const createdUser = await userModel.findById(user._id).select("-refreshToken -password")
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            `User ${user.email} could not be created.`
+        )
+    }
+
+    res.status(201).json(
+        new ApiResponse(
+            201,
+            user,
+            `User ${user.email} created successfully at ${user.createdAt}`
+        )
+    )
 
     // console.log(`\{\n${JS_IND}email: ${email},\n${JS_IND}username: ${username},\n${JS_IND}fullName: ${fullName},\n${JS_IND}password: ${password}\n\}`)
-    
+
 })
 
 export { registerUser }
--- src/utils/cloudinaryHandler.js
+++ src/utils/cloudinaryHandler.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
-import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from "../constants"
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } from "../constants.js"
 
 cloudinary.config({
 	cloud_name: CLOUDINARY_CLOUD_NAME,
